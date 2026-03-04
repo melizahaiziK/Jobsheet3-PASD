5536498bb80288cc03055d000eefae74823228ce
melizahaiziK <melizaha1zik@gmail.com>
2026-03-04 20:58:39 +0700

    Percobaan 3.3.1

--- Mahasiswa11.java
+++ Mahasiswa11.java
@@ -3,4 +3,11 @@ public class Mahasiswa11 {
     public String nim;
     public String kelas;
     public float ipk;
+
+     void cetakInfo() {
+        System.out.println("NIM   : " + nim);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
+    }
 }
