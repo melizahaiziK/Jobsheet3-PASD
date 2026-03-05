8a87b533031e177859aba466aa9d8ced59474741
melizahaiziK <melizaha1zik@gmail.com>
2026-03-05 18:30:26 +0700

    modifikasi percobaan 3.4.1 pertanyaan no.2

--- Matakuliah11.java
+++ Matakuliah11.java
@@ -4,7 +4,10 @@ public class Matakuliah11 {
     public int sks;
     public int jumlahJam;
 
-    public Matakuliah11 (String kode, String nama, int sks, int jumlahJam) {
+    public Matakuliah11 () {
+    }
+
+    void tambahData (String kode, String nama, int sks, int jumlahJam){
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
